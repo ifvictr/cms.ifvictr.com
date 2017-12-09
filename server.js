47eaf27796e6c3ae8008b59203e37626aee173dc
Victor Truong <me@ifvictr.com>
2017-12-09 22:19:23 +0000

    Format code, start converting into my own copy

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-var ghost = require('ghost');
-var cluster = require('cluster');
+const ghost = require("ghost");
+const cluster = require("cluster");
 
 // Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
-var WORKERS = process.env.WEB_CONCURRENCY || 1;
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-if (cluster.isMaster) {
-  // Master starts all workers and restarts them when they exit.
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`);
-    cluster.fork();
-  });
-  for (var i = 0; i < WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  // Run Ghost in each worker / processor core.
-  ghost().then(function (ghostServer) {
-    ghostServer.start();
-  });
+if(cluster.isMaster) {
+    // Master starts all workers and restarts them when they exit.
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`);
+        cluster.fork();
+    });
+    for(let i = 0; i < WORKERS; i++) {
+        cluster.fork();
+    }
+}
+else {
+    // Run Ghost in each worker/processor core.
+    ghost().then(server => {
+        server.start();
+    });
 }
