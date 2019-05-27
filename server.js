65600ad1e00950c5e8ebaae3e93abe17d315995c
Victor Truong <me@ifvictr.com>
2019-05-27 12:00:47 -0700

    Make Ghost single-process

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-const cluster = require('cluster')
 const ghost = require('ghost')
 
-// Heroku sets `WEB_CONCURRENCY` to the number of available processor cores.
-const WORKERS = process.env.WEB_CONCURRENCY || 1
-
-if (cluster.isMaster) {
-    // Master starts all workers and restarts them when they exit.
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Starting a new worker because PID: ${worker.process.pid} exited code ${code} from ${signal} signal.`)
-        cluster.fork()
+// Run a single Ghost process
+ghost()
+    .then(ghostServer => ghostServer.start())
+    .catch(e => {
+        console.error(`Ghost server error: ${e.message} ${e.stack}`)
+        process.exit(1)
     })
-    for (let i = 0; i < WORKERS; i++) {
-        cluster.fork()
-    }
-}
-else {
-    // Run Ghost in each worker/processor core.
-    ghost().then(server => server.start())
-}
